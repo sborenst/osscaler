46ca4a19cad6d0365e3659a0006a941afb5902ea
sborenst <shachar.borenstein@gmail.com>
2015-09-19 19:03:05 +1000

    Git hook check

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 
 import os
 import requests
+import csv
+import pprint
+import json
 
 from requests.auth import HTTPBasicAuth
 
@@ -9,8 +12,25 @@ from gunicorn.app.base import BaseApplication
 from gunicorn.six import iteritems
 
 def wsgi_handler(environ, start_response):
+
+
+    apiUrl = 'http://85.190.178.15:1936/haproxy?stats;csv'
+
+    auth = HTTPBasicAuth('admin', 'redhat')
+    r = requests.get( url=apiUrl, auth=auth )
+
+    fieldnames = ('pxname', 'svname', 'qcur', 'qmax', 'scur', 'smax', 'slim', 'stot', 'bin', 'bout', 'dreq', 'dresp', 'ereq', 'econ', 'eresp', 'wretr', 'wredis', 'status', 'weight', 'act', 'bck', 'chkfail', 'chkdown', 'lastchg', 'downtime', 'qlimit', 'pid', 'iid', 'sid', 'throttle', 'lbtot', 'tracked', 'type', 'rate', 'rate_lim', 'rate_max', 'check_status', 'check_code', 'check_duration', 'hrsp_1xx', 'hrsp_2xx', 'hrsp_3xx', 'hrsp_4xx', 'hrsp_5xx', 'hrsp_other', 'hanafail', 'req_rate', 'req_rate_max', 'req_tot', 'cli_abrt', 'srv_abrt', 'comp_in', 'comp_out', 'comp_byp', 'comp_rsp', 'lastsess', 'last_chk', 'last_agt', 'qtime', 'ctime', 'rtime', 'ttime', '')
+    reader = csv.DictReader(r.text.splitlines(), fieldnames)
+
+    out = '<html>Routes found:<br>'
+    for row in reader:
+        if row['pxname'].startswith('be_http_'):
+            out += row['pxname'].split('_', 2)[-1].replace('-','/',1) + '<br>'
+
+    out += '</html>'
+
     start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World!"]
+    return out
 
 class StandaloneApplication(BaseApplication):
     def __init__(self, app, options=None):
@@ -28,12 +48,25 @@ class StandaloneApplication(BaseApplication):
         return self.application
 
 if __name__ == '__main__':
-    # apiUrl = 'http://85.190.178.15:1936/haproxy?stats;csv'
+
+    apiUrl = 'http://85.190.178.15:1936/haproxy?stats;csv'
+
+    auth = HTTPBasicAuth('admin', 'redhat')
+    r = requests.get( url=apiUrl, auth=auth )
+
+    fieldnames = ('pxname', 'svname', 'qcur', 'qmax', 'scur', 'smax', 'slim', 'stot', 'bin', 'bout', 'dreq', 'dresp', 'ereq', 'econ', 'eresp', 'wretr', 'wredis', 'status', 'weight', 'act', 'bck', 'chkfail', 'chkdown', 'lastchg', 'downtime', 'qlimit', 'pid', 'iid', 'sid', 'throttle', 'lbtot', 'tracked', 'type', 'rate', 'rate_lim', 'rate_max', 'check_status', 'check_code', 'check_duration', 'hrsp_1xx', 'hrsp_2xx', 'hrsp_3xx', 'hrsp_4xx', 'hrsp_5xx', 'hrsp_other', 'hanafail', 'req_rate', 'req_rate_max', 'req_tot', 'cli_abrt', 'srv_abrt', 'comp_in', 'comp_out', 'comp_byp', 'comp_rsp', 'lastsess', 'last_chk', 'last_agt', 'qtime', 'ctime', 'rtime', 'ttime', '')
+    reader = csv.DictReader(r.text.splitlines(), fieldnames)
+
+
+    # out = ''
+    # for row in reader:
+    #     if row['pxname'].startswith('be_http_'):
+    #         out += row['pxname'].split('_', 2)[-1].replace('-','/',1) + '<br>'
+    # #
+    # # for row in reader:
+    # #     if row['pxname'].startswith('be_http_'):
+    # #         print( row['pxname'].split('_', 2)[-1].replace('-','/',1) )
     #
-    # # print( apiUrl )
-    # auth = HTTPBasicAuth('admin', 'redhat')
-    # r = requests.get( url=apiUrl, auth=auth )
-    #
-    # print( r.text )
-    #
+    # print(out)
+
     StandaloneApplication(wsgi_handler, {'bind': ':8080'}).run()
