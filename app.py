f14d24752deeb759f7ab846122eb2e0b4107f112
sborenst <shachar.borenstein@gmail.com>
2015-09-19 19:08:00 +1000

    Git hook check

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ from gunicorn.six import iteritems
 
 def wsgi_handler(environ, start_response):
 
-
-    apiUrl = 'http://85.190.178.15:1936/haproxy?stats;csv'
+    apiUrl = 'http://192.168.0.101:1936/haproxy?stats;csv'
 
     auth = HTTPBasicAuth('admin', 'redhat')
     r = requests.get( url=apiUrl, auth=auth )
@@ -49,13 +48,13 @@ class StandaloneApplication(BaseApplication):
 
 if __name__ == '__main__':
 
-    apiUrl = 'http://85.190.178.15:1936/haproxy?stats;csv'
-
-    auth = HTTPBasicAuth('admin', 'redhat')
-    r = requests.get( url=apiUrl, auth=auth )
-
-    fieldnames = ('pxname', 'svname', 'qcur', 'qmax', 'scur', 'smax', 'slim', 'stot', 'bin', 'bout', 'dreq', 'dresp', 'ereq', 'econ', 'eresp', 'wretr', 'wredis', 'status', 'weight', 'act', 'bck', 'chkfail', 'chkdown', 'lastchg', 'downtime', 'qlimit', 'pid', 'iid', 'sid', 'throttle', 'lbtot', 'tracked', 'type', 'rate', 'rate_lim', 'rate_max', 'check_status', 'check_code', 'check_duration', 'hrsp_1xx', 'hrsp_2xx', 'hrsp_3xx', 'hrsp_4xx', 'hrsp_5xx', 'hrsp_other', 'hanafail', 'req_rate', 'req_rate_max', 'req_tot', 'cli_abrt', 'srv_abrt', 'comp_in', 'comp_out', 'comp_byp', 'comp_rsp', 'lastsess', 'last_chk', 'last_agt', 'qtime', 'ctime', 'rtime', 'ttime', '')
-    reader = csv.DictReader(r.text.splitlines(), fieldnames)
+    # apiUrl = 'http://85.190.178.15:1936/haproxy?stats;csv'
+    #
+    # auth = HTTPBasicAuth('admin', 'redhat')
+    # r = requests.get( url=apiUrl, auth=auth )
+    #
+    # fieldnames = ('pxname', 'svname', 'qcur', 'qmax', 'scur', 'smax', 'slim', 'stot', 'bin', 'bout', 'dreq', 'dresp', 'ereq', 'econ', 'eresp', 'wretr', 'wredis', 'status', 'weight', 'act', 'bck', 'chkfail', 'chkdown', 'lastchg', 'downtime', 'qlimit', 'pid', 'iid', 'sid', 'throttle', 'lbtot', 'tracked', 'type', 'rate', 'rate_lim', 'rate_max', 'check_status', 'check_code', 'check_duration', 'hrsp_1xx', 'hrsp_2xx', 'hrsp_3xx', 'hrsp_4xx', 'hrsp_5xx', 'hrsp_other', 'hanafail', 'req_rate', 'req_rate_max', 'req_tot', 'cli_abrt', 'srv_abrt', 'comp_in', 'comp_out', 'comp_byp', 'comp_rsp', 'lastsess', 'last_chk', 'last_agt', 'qtime', 'ctime', 'rtime', 'ttime', '')
+    # reader = csv.DictReader(r.text.splitlines(), fieldnames)
 
 
     # out = ''
